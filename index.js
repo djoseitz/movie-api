63fe7be2ae39e8a3d569c311fc14c67c22128ab2
djoseitz <72769693+djoseitz@users.noreply.github.com>
2020-12-30 16:48:46 -0800

    remove authorization requirement from movies endpoint

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.get('/', (req, res) => {
 });
 
 // Return a list of all movies & data
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
