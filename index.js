11317ec491e9b6b6c84e15182567f3cd47ebde97
djoseitz <72769693+djoseitz@users.noreply.github.com>
2021-02-05 12:13:32 -0800

    removed CORS

--- index.js
+++ index.js
@@ -32,21 +32,21 @@ require('./passport');
 
 app.use(morgan('common'));
 
-const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://cinefandb.herokuapp.com', 'https://cinefan.netlify.app', '*'];
+// const cors = require('cors');
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://cinefandb.herokuapp.com', 'https://cinefan.netlify.app', '*'];
 
-const { check, validationResult } = require('express-validator');
+// const { check, validationResult } = require('express-validator');
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 // GET requests & app.use
 app.get('/', (req, res) => {
